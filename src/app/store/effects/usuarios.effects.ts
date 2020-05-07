3a718e867f39a40ba837d57e24c65ff4bf167456
David Toledo <dtoledoz0207@gmail.com>
2020-05-07 11:44:56 -0500

    catchErrors-on-usersEffects

--- src/app/store/effects/usuarios.effects.ts
+++ src/app/store/effects/usuarios.effects.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import * as usuariosActions from '../actions';
-import { tap, mergeMap, map } from 'rxjs/operators';
+import { tap, mergeMap, map, catchError } from 'rxjs/operators';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { of } from 'rxjs';
 
 
 @Injectable()
@@ -14,7 +15,8 @@ export class UsuariosEffects {
       ofType(usuariosActions.cargarUsuarios),
       mergeMap(
         () => this.usuarioService.getUsers().pipe(
-          map(users => usuariosActions.cargarUsuariosSuccess({usuarios: users}))
+          map(users => usuariosActions.cargarUsuariosSuccess({usuarios: users})),
+          catchError( err => of(usuariosActions.cargarUsuariosError({payload: err})) )
         )
       )
     )
