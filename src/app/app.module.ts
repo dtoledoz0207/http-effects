eee2fdad58153e487e6807e3c166c3b1546e0432
David Toledo <dtoledoz0207@gmail.com>
2020-05-07 11:09:48 -0500

    effects-installed-create-first-usuariosEffect

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,10 @@ import { UsuariosModule } from './usuarios/usuarios.module';
 //Ngrx
 import { StoreModule } from '@ngrx/store';
 import {appReducers} from './store/app.reducers';
+import { EffectsModule } from '@ngrx/effects';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import {environment} from '../environments/environment';
+import { EffectsArray } from './store/effects';
 
 @NgModule({
   declarations: [
@@ -25,6 +27,7 @@ import {environment} from '../environments/environment';
     AppRoutingModule,
     HttpClientModule,
     StoreModule.forRoot(appReducers),
+    EffectsModule.forRoot(EffectsArray),
     StoreDevtoolsModule.instrument({
       maxAge: 25, // Retains last 25 states
       logOnly: environment.production, // Restrict extension to log-only mode
--- src/app/usuarios/lista/lista.component.ts
+++ src/app/usuarios/lista/lista.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 //import { UsuarioService } from 'src/app/services/usuario.service';
 import { Usuario } from 'src/app/models/usuario.model';
+import { Store } from '@ngrx/store';
+import { AppState } from 'src/app/store/app.reducers';
+import { cargarUsuarios } from 'src/app/store/actions';
 
 @Component({
   selector: 'app-lista',
@@ -11,9 +14,10 @@ export class ListaComponent implements OnInit {
 
   usuarios: Usuario[] = [];
 
-  constructor() { }
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit() {
+    this.store.dispatch(cargarUsuarios())
     /* this.usuarioService.getUsers().subscribe(users => {
       console.log(users);
       this.usuarios = users;
